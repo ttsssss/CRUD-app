b950331b0d26b3a453b6c3ddc582940780efa4d9
ttsssss <sandovaltrent@gmail.com>
2021-09-14 10:07:44 -0600

    Changes

--- app.js
+++ app.js
@@ -3,3 +3,20 @@
 const express = require('express');
 //route files
 const routes = require 
+
+//I Set up environment
+//II Set up MySqL and App
+//III Add porjects from databases
+//IV Display Projects
+// V Edit and Delete Projects
+//
+
+const express = require ('express');
+const path = require ('path');
+const bodyParser = require('bodyParser');
+const exphbs = require ('express-handlebars');
+const expressvalidator = ('')
+
+//Routes Files
+const routes = require('');
+const routes = require()
---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'shoulderlean',
+    database: 'portfolio'
+});
+connection.connect();
+router.get('/', function(req, res, next) {
+    connection.query("SELECT * FROM projects", function(err, rows, fields){
+        if(err) throw err;
+        res.render('index', {
+            "projects": rows
+        });
+    });
+});
+router.get('/details/:id', function(req, res, next) {
+    connection.query("SELECT * FROM projects WHERE id = ?", req.params.id, function(err, rows, fields){
+        if(err) throw err;
+        res.render('details', {
+            "projects": rows[0]
+        });
+    });
+});
+module.exports = router;
